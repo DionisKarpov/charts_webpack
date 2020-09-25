e47982abcacaaa4fde69f35a94dfae22aedfd91e
Dionis Karpov <dionis.karpov@gmail.com>
2020-09-25 21:59:10 +0300

    Move JS Lib to html

--- src/app/chart.js
+++ src/app/chart.js
@@ -1,12 +1,4 @@
 import '../styles/styles.scss';
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
-import { Swappable } from '@shopify/draggable';
-
-const swappable = new Swappable(document.querySelectorAll('.swappable'), {
-	draggable: 'li'
-});
 
 class TestClass {}
 
